89b4cd18bacedca42511dcca004528f804bb0fcd
jorgemc94 <jmaciasc1994@gmail.com>
2024-05-21 09:27:05 +0200

    ADD FooterComponents

---
+++ src/components/FooterComponents/FooterComponent.jsx
@@ -0,0 +1,14 @@
+
+import './FooterComponent.css'
+
+export const FooterComponent = () => {
+
+
+    return (
+        <>
+            <footer className="FooterComponent">
+                <p className="FooterComponent__text">ALLPHOTOS @COPYRIGHT 2024</p>
+            </footer>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './styles.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { FooterComponent } from './components/FooterComponents/FooterComponent'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='' element={<FooterComponent/>}/>
+      </Routes>
+    </BrowserRouter>
     
   </React.StrictMode>,
 )
