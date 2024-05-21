8aac87251f6f81c0bbe049f3ea09e32f3c5d46c4
jorgemc94 <jmaciasc1994@gmail.com>
2024-05-21 13:45:02 +0200

    ADD HeaderComponents

---
+++ src/components/HeaderComponents/HeaderComponent.jsx
@@ -0,0 +1,14 @@
+
+import { NavbarComponent } from '../NavbarComponents/NavbarComponent'
+import './HeaderComponent.css'
+
+export const HeaderComponent = () => {
+
+    return (
+        <>
+            <header className='HeaderComponent'>
+                <NavbarComponent/>
+            </header>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { FooterComponent } from './components/FooterComponents/FooterComponent'
-import { NavbarComponent } from './components/NavbarComponents/NavbarComponent'
+import { HeaderComponent } from './components/HeaderComponents/HeaderComponent'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='' element={<NavbarComponent/>}/>
-        <Route path='' element={<FooterComponent/>}/>
+        <Route path='' element={<HeaderComponent/>}/>
       </Routes>
     </BrowserRouter>
     
