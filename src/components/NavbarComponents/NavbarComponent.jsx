f4d978205126544c293aab8a6058dbcab234ca47
jorgemc94 <jmaciasc1994@gmail.com>
2024-05-21 10:16:59 +0200

    ADD NavbarComponents

---
+++ src/components/NavbarComponents/NavbarComponent.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom"
+import './NavbarComponent.css'
+
+export const NavbarComponent = () => {
+
+
+    return (
+        <>
+            <nav className='NavbarComponent'>
+                <div className='NavbarComponent__logo'>
+                    <NavLink to={''} className='NavbarComponent__logo__text'>ALL PHOTOS</NavLink>
+                </div>
+                <div className='NavbarComponent__links'>
+                    <ul className='NavbarComponent__links__list'>
+                        <NavLink to={'/FavoritePhotos'} className='NavbarComponent__links__list__item'>FavoritePhotos</NavLink>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom/client'
 import './styles.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { FooterComponent } from './components/FooterComponents/FooterComponent'
+import { NavbarComponent } from './components/NavbarComponents/NavbarComponent'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path='' element={<NavbarComponent/>}/>
         <Route path='' element={<FooterComponent/>}/>
       </Routes>
     </BrowserRouter>
