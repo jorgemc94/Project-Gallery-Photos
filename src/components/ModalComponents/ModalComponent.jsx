402058ae13560ff4a412ca30da8ad6a9107f2ea7
jorgemc94 <jmaciasc1994@gmail.com>
2024-05-27 23:09:41 +0200

    Error edit description

--- src/components/ModalComponents/ModalComponent.jsx
+++ src/components/ModalComponents/ModalComponent.jsx
@@ -1,25 +1,25 @@
 
+import { Alert } from '@mui/material'
 import './ModalComponent.css'
 
 export const ModalComponent = ({isOpen, onClose, description, width, height, likes, date, onSubmit}) => {
 
-
     return(
         <>
-            <div className='ModalComponent'>
-                <span className="material-symbols-outlined ModalComponent__Close" onClick={onClose}> cancel </span>
-                <h2 className="ModalComponent__Title">Desciption</h2>
-                <textarea className="ModalComponent__Description" placeholder={description}></textarea>
-                <ul className="ModalComponent__list">
-                    <li className="ModalComponent__list__item"><p className="ModalComponent__list__item__definicion">Width: </p>{width}</li>
-                    <li className="ModalComponent__list__item"><p className="ModalComponent__list__item__definicion">Height: </p>{height}</li>
-                    <li className="ModalComponent__list__item"><p className="ModalComponent__list__item__definicion">Likes: </p>{likes}</li>
-                    <li className="ModalComponent__list__item"><p className="ModalComponent__list__item__definicion">Date: </p>{date}</li>
-                </ul>
-                <form open={isOpen} className='ModalComponent__form' onSubmit={onSubmit}>
+            <form open={isOpen} className='ModalComponent__form' onSubmit={onSubmit}>
+                <div className='ModalComponent'>
+                    <span className="material-symbols-outlined ModalComponent__Close" onClick={onClose}> cancel </span>
+                    <h2 className="ModalComponent__Title">Desciption</h2>
+                    <textarea className="ModalComponent__Description" placeholder={description}></textarea>
+                    <ul className="ModalComponent__list">
+                        <li className="ModalComponent__list__item"><p className="ModalComponent__list__item__definicion">Width: </p>{width}</li>
+                        <li className="ModalComponent__list__item"><p className="ModalComponent__list__item__definicion">Height: </p>{height}</li>
+                        <li className="ModalComponent__list__item"><p className="ModalComponent__list__item__definicion">Likes: </p>{likes}</li>
+                        <li className="ModalComponent__list__item"><p className="ModalComponent__list__item__definicion">Date: </p>{date}</li>
+                    </ul>
                     <button type='submit' className='ModalComponent__form__send'>Send</button>
-                </form>
-            </div>
+                </div>
+            </form>
         </>
     )
 }
